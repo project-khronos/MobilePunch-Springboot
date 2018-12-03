13bdac34d4aced9c92246da48273121586a0de10
Alex Benedict <20505631+abenedic@users.noreply.github.com>
2018-12-03 10:18:49 -0700

    Added basic OAuth, no user isolation yet.

--- src/main/java/edu/cnm/projectkronos/MobilePunchBackEndApplication.java
+++ src/main/java/edu/cnm/projectkronos/MobilePunchBackEndApplication.java
@@ -1,12 +1,35 @@
 package edu.cnm.projectkronos;
 
+import com.google.api.client.util.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
+@EnableWebSecurity
+@EnableResourceServer
 @SpringBootApplication
-public class MobilePunchBackEndApplication {
+public class MobilePunchBackEndApplication extends ResourceServerConfigurerAdapter {
+
+  @Value("${oauth.clientId}")
+  private String clientId;
 
   public static void main(String[] args) {
     SpringApplication.run(MobilePunchBackEndApplication.class, args);
+  }
+
+  @Override
+  public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+    resources.resourceId(clientId);
+  }
+
+  @Override
+  public void configure(HttpSecurity http) throws Exception {
+    http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    http.authorizeRequests().anyRequest().hasRole("USER");
   }
 }
