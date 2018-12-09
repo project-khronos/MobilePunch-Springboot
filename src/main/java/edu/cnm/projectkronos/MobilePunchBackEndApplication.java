a42cb5833c45ed3f92711898bdfb1d3ffe4d66c1
Travis Decker <decker4693@gmail.com>
2018-12-08 17:55:59 -0700

    Re enabled Oauth

--- src/main/java/edu/cnm/projectkronos/MobilePunchBackEndApplication.java
+++ src/main/java/edu/cnm/projectkronos/MobilePunchBackEndApplication.java
@@ -4,34 +4,34 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-//import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
-//@EnableWebSecurity
-//@EnableResourceServer
+@EnableWebSecurity
+@EnableResourceServer
 @SpringBootApplication
 @EnableScheduling
-public class MobilePunchBackEndApplication {
-//extends ResourceServerConfigurerAdapter
-//  @Value("${oauth.clientId}")
-//  private String clientId;
+public class MobilePunchBackEndApplication extends ResourceServerConfigurerAdapter {
+
+  @Value("${oauth.clientId}")
+  private String clientId;
 
   public static void main(String[] args) {
     SpringApplication.run(MobilePunchBackEndApplication.class, args);
   }
 
-//  @Override
-//  public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-//    resources.resourceId(clientId);
-//  }
+  @Override
+  public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+    resources.resourceId(clientId);
+  }
 
-//  @Override
-//  public void configure(HttpSecurity http) throws Exception {
-//    http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//    http.authorizeRequests().anyRequest().hasRole("USER");
-//  }
+  @Override
+  public void configure(HttpSecurity http) throws Exception {
+    http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    http.authorizeRequests().anyRequest().hasRole("USER");
+  }
 }
